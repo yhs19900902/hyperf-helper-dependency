5d82d606ced5ba6a25b01bb32429bb1a8ef766b9
yhd575 <517691646@qq.com>
2024-06-28 14:34:40 +0800

    first commit

---
+++ src/Annotation/Dependency.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HyperfHelper\Dependency\Annotation;
+
+
+use Attribute;
+use Hyperf\Di\Annotation\AnnotationCollector;
+use Hyperf\Di\Annotation\AnnotationInterface;
+use HyperfHelper\Dependency\Annotation\Collector\DependencyCollector;
+use ReflectionException;
+
+#[Attribute(Attribute::TARGET_CLASS)]
+class Dependency implements AnnotationInterface
+{
+
+    /**
+     * @param string<class-string> $identifier 接口类
+     * @param int $priority 权重
+     */
+    public function __construct(public string $identifier = '', protected int $priority = 1)
+    {
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function collectClass(string $className): void
+    {
+        DependencyCollector::collectorDependency($className, $this->identifier, $this->priority);
+    }
+
+    public function collectClassConstant(string $className, ?string $target): void
+    {
+        AnnotationCollector::collectClassConstant($className, $target, static::class, $this);
+    }
+
+    public function collectMethod(string $className, ?string $target): void
+    {
+    }
+
+    public function collectProperty(string $className, ?string $target): void
+    {
+    }
+}
